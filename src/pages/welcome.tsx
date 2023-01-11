77fdcb84fbbb0150410ebabd6b98385d011e32fb
timothyliutl <18tfl1@queensu.ca>
2023-01-11 15:16:02 -0500

    updated quick links

--- src/pages/welcome.tsx
+++ src/pages/welcome.tsx
@@ -1,7 +1,10 @@
-import { AppBar, Box, Button, Card, CardContent, Container, Divider, Grid, List, ListItem, ListItemButton, ListItemText, Toolbar, Typography } from '@mui/material';
+import { AppBar, Box, Button, Card, CardContent, Container, Divider, Grid, Icon, List, ListItem, ListItemButton, ListItemText, Toolbar, Typography } from '@mui/material';
 
 
 import React from 'react'
+import ArticleIcon from '@mui/icons-material/Article';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 
 
@@ -34,28 +37,54 @@ const Welcome = () => {
                 </Toolbar>
             </AppBar>
             <div style={{ marginTop: '70px' }}>
-                <Container>
+                <Container style={{ width: '70%' }}>
                     <Typography variant='h2' textAlign={'center'}> Hello I'm Timothy Liu </Typography>
                     <Typography variant='h5' textAlign={'center'}> A Fourth Year Mathematics and Engineering Student</Typography>
 
-                    <div style={{marginTop: '200px'}}>
-                    
+                    <div style={{ marginTop: '75px' }}>
+
                     </div>
-                    <Typography variant='h4'>Quick Links</Typography>
-                    <Grid container direction={'row'} justifyContent='space-evenly'>
-                        <Card sx={{minWidth:'275px'}}>
+                    <Grid container direction={'row'} justifyContent='space-evenly' style={{ marginTop: '30px' }}>
+                        <Card sx={{ minWidth: '220px', paddingTop: '10px' }}>
                             <CardContent>
-                                Hello world
+                                <Grid container justifyContent={'space-between'}>
+                                    <Grid item>
+                                        <ArticleIcon color='primary' fontSize='large'></ArticleIcon>
+                                    </Grid>
+                                    <Grid item>
+                                        <Typography variant='h6' textAlign={'center'}>
+                                            Resume
+                                        </Typography>
+                                    </Grid>
+                                </Grid>
                             </CardContent>
                         </Card>
-                        <Card sx={{minWidth:'275px'}}>
-                            <CardContent>
-                                Hello world
+                        <Card sx={{minWidth:'220px', paddingTop: '10px' }}>
+                        <CardContent>
+                                <Grid container justifyContent={'space-between'}>
+                                    <Grid item>
+                                        <GitHubIcon color='primary' fontSize='large'></GitHubIcon>
+                                    </Grid>
+                                    <Grid item>
+                                        <Typography variant='h6'>
+                                            GitHub
+                                        </Typography>
+                                    </Grid>
+                                </Grid>
                             </CardContent>
                         </Card>
-                        <Card sx={{minWidth:'275px'}}>
-                            <CardContent>
-                                Hello world
+                        <Card sx={{ minWidth: '220px', paddingTop: '10px' }}>
+                        <CardContent>
+                                <Grid container justifyContent={'space-between'}>
+                                    <Grid item>
+                                        <LinkedInIcon color='primary' fontSize='large'></LinkedInIcon>
+                                    </Grid>
+                                    <Grid item>
+                                        <Typography variant='h6'>
+                                            LinkedIn
+                                        </Typography>
+                                    </Grid>
+                                </Grid>
                             </CardContent>
                         </Card>
                     </Grid>
