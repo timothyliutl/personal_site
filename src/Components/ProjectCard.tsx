737b71e3bc1f6ef9361ebeb73de9a2cde084670f
timothyliutl <18tfl1@queensu.ca>
2023-05-31 20:39:24 -0400

    fixed align issue

--- src/Components/ProjectCard.tsx
+++ src/Components/ProjectCard.tsx
@@ -1,46 +1,46 @@
 import { Button, Card, CardActions, CardContent, CardMedia, Typography } from "@mui/material";
 import React from "react";
 
-interface propsData{
-//structure of data we need for the card
-title: string,
-description:string,
-image?: string,
-github?: string,
-liveDemo?: string
+interface propsData {
+    //structure of data we need for the card
+    title: string,
+    description: string,
+    image?: string,
+    github?: string,
+    liveDemo?: string
 }
 
-interface actionProps{
+interface actionProps {
     text?: string,
     link?: string
 }
 
-const ActionButton = (props:actionProps) =>{
-    if(props.text && props.link){
-        return(
+const ActionButton = (props: actionProps) => {
+    if (props.text && props.link) {
+        return (
             <Button target="_blank" size="small" href={props.link}>{props.text}</Button>
         )
-    }else{
-        return(<div></div>)
+    } else {
+        return (<div></div>)
     }
 }
 
-const ProjectCard = (props:propsData) =>{
+const ProjectCard = (props: propsData) => {
 
-    return(
-        <div>
-            <Card variant="outlined" sx={{width:'345px'}}>
-                <CardMedia image={props.image} sx={{height:'140px'}}></CardMedia>
-                <CardContent>
-                    <Typography variant="h5">{props.title}</Typography>
-                    <Typography variant="body2">{props.description}</Typography>
-                </CardContent>
-                <CardActions>
-                    <ActionButton link={props.liveDemo} text="Live Demo"></ActionButton>
-                    <ActionButton link={props.github} text="GitHub"></ActionButton>
-                </CardActions>
-            </Card>
-        </div>
+    return (
+
+        <Card variant="outlined" sx={{ width: '345px', height:'100%'}}>
+            <CardMedia image={props.image} sx={{ height: '140px' }}></CardMedia>
+            <CardContent>
+                <Typography variant="h5">{props.title}</Typography>
+                <Typography variant="body2">{props.description}</Typography>
+            </CardContent>
+            <CardActions>
+                <ActionButton link={props.liveDemo} text="Live Demo"></ActionButton>
+                <ActionButton link={props.github} text="GitHub"></ActionButton>
+            </CardActions>
+        </Card>
+
     )
 }
 
--- src/PageElements/Projects.tsx
+++ src/PageElements/Projects.tsx
@@ -38,18 +38,26 @@ const Projects = () => {
         image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/2048px-Spotify_logo_without_text.svg.png',
         github:'https://github.com/timothyliutl/personal_site',
         liveDemo:'https://www.timothyliu.dev/spotiwhy'
-    }]
+    },
+    {
+        name: 'Valorant E-Sports Predictions',
+        description: 'Using Machine Learning to predict the outcome of professional Valorant matches',
+        image: 'https://cdn.mos.cms.futurecdn.net/mJVvYfj86AtHd3f4uZBEcX-1200-80.jpg',
+        github:'https://github.com/timothyliutl/ValorantData',
+        liveDemo:''
+    },
+    ]
 
     return (
         <div style={{ minHeight: '100vh', backgroundColor: '#023E8A', paddingBottom:'100px' }}>
             <div style={{ width: '80%', marginLeft: 'auto', marginRight: 'auto', paddingTop: '200px' }}>
                 <Typography style={{ marginTop: '10px' }} textAlign={'center'} fontFamily={'Open Sans'} color={'white'} variant="subtitle1">My Recent Work</Typography>
                 <Typography textAlign={'center'} variant="h3" fontFamily={'Open Sans'} color={'#caf0f8'}>Projects</Typography>
-                <Grid style={{marginTop:'50px'}} container spacing={3}>
+                <Grid style={{marginTop:'50px'}} container spacing={3} justifyContent={'center'}>
                     {
                         projects.map((data) => {
                             return (
-                                <Grid xs={4} item>
+                                <Grid sm={6} md={4} item justifyContent={'center'}>
                                     <ProjectCard title={data.name} description={data.description} image={data.image} liveDemo={data.liveDemo} github={data.github}></ProjectCard>
                                 </Grid>
                             )
