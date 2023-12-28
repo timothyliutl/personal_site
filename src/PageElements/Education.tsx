2f7cdf8001b5c7bb4266f21df385291fffbb9ebd
timothyliutl <18tfl1@queensu.ca>
2023-12-28 14:16:17 -0500

    hide education for now

--- src/PageElements/Education.tsx
+++ src/PageElements/Education.tsx
@@ -8,7 +8,7 @@ interface props {
 const Education = (props: props) => {
 
     return (
-        <div style={{ minHeight: '100vh', backgroundColor: '#023E8A', paddingBottom: '100px', marginTop: '0px', paddingTop: '100px' }}>
+        <div style={{ minHeight: '100vh', backgroundColor: '#023E8A', paddingBottom: '100px', marginTop: '0px', paddingTop: '100px', display: 'none' }}>
             <hr style={{ width: '30%', marginBottom: '100px' }}></hr>
             <Typography style={{ marginTop: '10px' }} textAlign={'center'} fontFamily={'Open Sans'} color={'white'} variant="subtitle1">My Academic Journey</Typography>
             <Typography textAlign={'center'} variant="h3" fontFamily={'Open Sans'} color={'#caf0f8'}>Education</Typography>
